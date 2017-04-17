9e50c4e471f48a628942169880050eb98de4731e
Admin <Admin@ADMINMI-CSVGNI7>
2017-04-17 07:04:06 +0700

    ~ Refactoring.

---
+++ HappyLifeManagement/Scripts/app/replace-k-in-amount.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    $(function () {
+        //Utility when input amount of money.
+        $('#Amount').on('blur', function () {
+            $(this).val($(this).val().replace('k', '000')
+                                     .replace('K', '000'));
+        });
+    })
+})();
